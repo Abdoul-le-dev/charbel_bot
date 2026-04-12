0344e11658645a48fe9dab952fd4bd1e682ff878
Abdoul-le-dev <abdoulledev@gmail.com>
2026-04-13 01:25:06 +0200

    suite

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 from database.database import init_db, upsert_user, log_member, get_file_id, save_file_id
 
-TOKEN = ""
+TOKEN = "8609131464:AAGK5k1jkLJvY1OSvHcR3YPnwqEqOFeWuAs"
 
 ADMIN_IDS      = {6992809421, 6799962131}
 ADMIN_USERNAME = "@Faiseur2Rois"
